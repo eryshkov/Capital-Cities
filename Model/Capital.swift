fb39fc47e6455905615f868639bbe2ffbbd8380c
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-21 10:55:43 +0300

    mvc folders created

---
+++ Model/Capital.swift
@@ -0,0 +1,13 @@
+//
+//  Capital.swift
+//  Capital Cities
+//
+//  Created by Evgeniy Ryshkov on 21/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class Capital: NSObject {
+
+}
