211c99adcd9bc0803dfc60763c81f25918e19895
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-21 09:49:50 +0300

    Initial Commit

---
+++ Capital Cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Capital Cities
+//
+//  Created by Evgeniy Ryshkov on 21/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
